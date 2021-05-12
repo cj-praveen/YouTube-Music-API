3faf74d2fc6f74704baed4e3802d88e37ea9b3dd
Saptak Bhoumik <saptakbhoumik@gmail.com>
2021-05-12 18:54:58 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,8 @@
 import requests
 import os
 import pafy
-import socket
 import webbrowser
 
-Host = socket.gethostname()
-IP = socket.gethostbyname(Host)
-
-if "127.0.0.1" in IP:
-    print("Your Are Offline, Please Connect to Internet!")
-    exit()
-else:
-    pass
 
 def download(song_name, path):
     if "Topic" in song_name.lower():
@@ -50,3 +41,4 @@ def getURL(song_name):
     GET_content = requests.get(PyWhatKitAPI)
     Music_URL = GET_content.text.replace("www.", "music.")
     print(Music_URL)
+    return Music_Url
