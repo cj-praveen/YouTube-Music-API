8d1a27ec191221c07befc9033ea2522c8156ed74
Sijey <80583308+sijey-praveen@users.noreply.github.com>
2021-05-26 04:54:10 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,45 @@
-import requests
+"""
+MIT License
+
+Copyright (c) 2021 Sijey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import os
 import pafy
 import socket
 import webbrowser
+from youtube_search import YoutubeSearch
+
+def Search(song_name):
+    query = YoutubeSearch(song_name, max_results=1).to_dict()
+    Data = query[0]
+    Music_ID = Data['url_suffix']
+    URL = f"https://music.youtube.com{Music_ID}"
+    return URL
 
 Host = socket.gethostname()
 IP = socket.gethostbyname(Host)
 
 if "127.0.0.1" in IP:
-    print("Your Are Offline, Please Connect to Internet!")
+    print("You're Offline, Please Connect to Internet!")
     exit()
 else:
     pass
@@ -17,12 +48,9 @@ def download(song_name, path):
     if "Topic" in song_name.lower():
         pass
     else:
-        song_name = song_name + "Album Topic"
-    PyWhatKitAPI = "https://pywhatkit.herokuapp.com/playonyt?topic=" + song_name
+        song_name = song_name + " Album Topic"
     print("Connecting To Youtube Music...")
-    GET_content = requests.get(PyWhatKitAPI)
-    Music_URL = GET_content.text.replace("www.", "music.")
-    dl_config = pafy.new(GET_content.text)
+    dl_config = pafy.new(Search(song_name))
     print("\nCollecting Data From Youtube Music...")
     dl = dl_config.getbestaudio()
     dl.download(path)
@@ -32,35 +60,17 @@ def play(song_name):
     if "Topic" in song_name.lower():
         pass
     else:
-        song_name = song_name + "Album Topic"
-    PyWhatKitAPI = "https://pywhatkit.herokuapp.com/playonyt?topic=" + song_name
+        song_name = song_name + " Album Topic"
     print("Connecting To Youtube Music...")
-    GET_content = requests.get(PyWhatKitAPI)
-    Music_URL = GET_content.text.replace("www.", "music.") + ("&list=RDAMVMd0eUMnJ5kTA")
-    song = pafy.new(GET_content.text)
-    print("\nNow Playing : " + song.title + " | Copyrights (c) " + song.author)
+    Music_URL = Search(song_name)
     webbrowser.open(Music_URL)
-
-def getURL(song_name):
-    if "Topic" in song_name.lower():
-        pass
-    else:
-        song_name = song_name + "Album Topic"
-    PyWhatKitAPI = "https://pywhatkit.herokuapp.com/playonyt?topic=" + song_name
-    GET_content = requests.get(PyWhatKitAPI)
-    Music_URL = GET_content.text.replace("www.", "music.")
-    print(Music_URL)
     return Music_URL
 
 def playonvlc(song_name):
     if "Topic" in song_name.lower():
         pass
     else:
-        song_name = song_name + "Album Topic"
+        song_name = song_name + " Album Topic"
     print("Connecting To Youtube Music...")
-    PyWhatKitAPI = "https://pywhatkit.herokuapp.com/playonyt?topic=" + song_name
-    GET_content = requests.get(PyWhatKitAPI)
-    Music_URL = GET_content.text
-    song = pafy.new(GET_content.text)
-    print("\nNow Playing : " + song.title + " | Copyrights (c) " + song.author)
+    Music_URL = Search(song_name)
     os.system("start vlc " + Music_URL)
