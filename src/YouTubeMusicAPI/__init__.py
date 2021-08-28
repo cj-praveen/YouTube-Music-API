9c19126fc0bdb58ea60232a22db98ebee284b15c
Sijey <80583308+sijey-praveen@users.noreply.github.com>
2021-08-28 07:23:05 -0700

    Update __init__.py

--- src/YouTubeMusicAPI/__init__.py
+++ src/YouTubeMusicAPI/__init__.py
@@ -1,76 +1,71 @@
-"""
-YouTubeMusicAPI Library
-~~~~~~~~~~~~~~~~~~~~~
-
-An unofficial YouTube Music API For Python Programming Language. Written in Python, for human beings.
-"""
-
-__all__ = ["get_track"]
-
+__all__ = ["YouTubeMusicAPI"]
 __author__ = "Sijey (cjpraveen@hotmail.com)"
-__version__ = ""
+__version__ = "2.1"
 __copyright__ = "Copyright (c) 2021 Sijey"
-__license__ = "MIT" # Use of this source code is governed by the MIT License license, See LICENSE for more details.
+__license__ = "MIT" # Use of this source code is governed by the MIT License, See LICENSE for more details. >>> https://github.com/sijey-praveen/YouTube-Music-API/blob/Sijey/LICENSE
 
 from os import *
 from requests import *
 import platform
 from bs4 import *
 
-class YouTubeMusicAPI():
-    def __init__(self, song_name : str):
-        self.song_name = song_name
+def YouTubeMusicAPI(query : str, type : str):
 
-        """
-        Get's the MusicId or VideoId or TrackId from youtube using 'Request' library.
-        """
-        count = 0
-        results = str(get(f"https://www.youtube.com/results?q={self.song_name} Album Topic").content).split('"')
+    """
 
-        for i in results:
-            count += 1
-            if i == 'WEB_PAGE_TYPE_WATCH' : break
-        if "watch" in results[count - 5] : self.music_id = results[count - 5]
-        else: print("No Song found."), exit()
+    YouTubeMusicAPI Library
+    ~~~~~~~~~~~~~~~~~~~~~
 
-        """
-        Gather's All Metadata about the music or song or track using web scraping libraries ('BeautifulSoup' & 'Requests' library used here for this process.).
+    An unofficial YouTube Music API For Python Programming Language. Written in Python, for human beings.
+    
+    >>> https://github.com/sijey-praveen/YouTube-Music-API 
 
-        """
+    """
 
-        __src__ = BeautifulSoup(get(f"https://www.youtube.com/{self.music_id}").text, 'html.parser')
-        self.track_name = __src__.find("meta", attrs={"name" : "title"}).get("content")
-        self.datePublished = __src__.find("meta", attrs={"itemprop" : "datePublished"}).get("content")
-        self.interactionCount = __src__.find("meta", attrs={"itemprop" : "interactionCount"}).get("content")
-        self.artist_id = __src__.find("meta", attrs={"itemprop" : "channelId"}).get("content")
-        self.track_Id = __src__.find("meta", attrs={"itemprop" : "videoId"}).get("content")
-        self.artist_image = BeautifulSoup(get(f"https://www.youtube.com/channel/{self.artist_id}").text, 'html.parser').find("link", attrs={"itemprop" : "thumbnailUrl"}).get("href")
-        for i in __src__.find_all("span")[0]:
-            if i.get("itemprop") == "name" : self.artist = str(i.get("content"))
+    base_url = f"https://www.youtube.com/results?q={query}"
 
-    def get_track(self):
-        """
-        >>> .get_track()
+    if type == "track" : snippet, _type = "watch", "WEB_PAGE_TYPE_WATCH"
+    elif type == "artist" : snippet, _type = "channel", "WEB_PAGE_TYPE_CHANNEL"
+    elif type == "playlist" : snippet, _type, base_url = "watch", "WEB_PAGE_TYPE_WATCH", base_url + "&sp=EgIQAw%253D%253D"
+    else : print("Invalid Search Type, ['artist', 'playlist', 'track'] are available."), exit()
 
-        Return metadata about the song or music or track in json format based on your search query.
-        """
+    count, results = 0, str(get(base_url).content).split('"')
+    for i in results:
+        count += 1
+        if i == _type : break
+    if snippet in results[count - 5] : id = results[count - 5]
+    else: print(f"No result found."), exit()
+
+    if type == "track":
+
+        __src__ = BeautifulSoup(get(f"https://www.youtube.com/{id}").text, 'html.parser')
+        track_Id = __src__.find("meta", attrs={"itemprop" : "videoId"}).get("content")
         return dict(
-        track_name = self.track_name, 
-        datePublished = self.datePublished,
-        track_Id = self.track_Id,
-        track_url = f"https://music.youtube.com/watch?v={self.track_Id}",
-        track_image = f"https://i.ytimg.com/vi/{self.track_Id}/hqdefault.jpg", 
-        interactionCount = self.interactionCount, 
-        artist = self.artist, 
-        artist_url = f"https://music.youtube.com/channel/{self.artist_id}",
-        artist_id = self.artist_id, 
-        artist_image = self.artist_image
+        track_name = __src__.find("meta", attrs={"name" : "title"}).get("content"), 
+        datePublished = __src__.find("meta", attrs={"itemprop" : "datePublished"}).get("content"),
+        track_Id = track_Id,
+        track_url = f"https://music.youtube.com/watch?v={track_Id}",
+        track_image = f"https://i.ytimg.com/vi/{track_Id}/hqdefault.jpg", 
+        )
+
+    if type == "artist":
+
+        return dict(
+        artist = BeautifulSoup(get(f"https://www.youtube.com{id}").text, 'html.parser').find("meta", attrs={"property" : "og:title"}).get("content"),
+        artist_url = f"https://music.youtube.com{id}",
+        artist_id = id.split("/")[2],
+        artist_image = BeautifulSoup(get(f"https://www.youtube.com{id}").text, 'html.parser').find("link", attrs={"itemprop" : "thumbnailUrl"}).get("href")
     )
 
+    if type == "playlist":
+
+        return dict(
+            playlist_name = BeautifulSoup(get("https://music.youtube.com/playlist?list={}".format(id.split("list=")[1])).text, 'html.parser').find("meta", attrs={"property" : "og:title"}).get("content"),
+            playlist_url = "https://music.youtube.com/playlist?list={}".format(id.split("list=")[1]),
+            playlist_id = id.split("list=")[1]
+        )
+
 if __name__ == "__main__":
-
-    # Check for new version update
-
-    if 200 == head("https://pypi.org/project/YouTubeMusicAPI/2.1/").status_code:
-        if "windows" == platform.system().lower() : system("pip install YouTubeMusicAPI==2.1")
-        else : system("pip3 install YouTubeMusicAPI==2.1")
+    if 200 == head("https://pypi.org/project/YouTubeMusicAPI/2.2/").status_code:
+        if "windows" == platform.system().lower() : system("pip install YouTubeMusicAPI==2.2")
+        else : system("pip3 install YouTubeMusicAPI==2.2")
