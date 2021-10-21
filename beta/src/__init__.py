360cae40ba89838bd35efaf436639f66904765ab
Sijey <cjpraveen@hotmail.com>
2021-10-21 11:20:17 -0700

    Update __init__.py

--- beta/src/__init__.py
+++ beta/src/__init__.py
@@ -1,54 +1,35 @@
-__all__ = ["YouTubeMusicAPI"]
-__author__ = "Sijey (cjpraveen@hotmail.com)"
-__version__ = "beta"
-__copyright__ = "Copyright (c) 2021 Sijey"
-__license__ = "MIT" # >>> https://mit-license.org/
-
 from requests import get
-from bs4 import BeautifulSoup
 from warnings import filterwarnings
+from flask import Flask, request
+from flask_cors import cross_origin, CORS
 
-class YouTubeMusicAPI():
+app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
+user_agent = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0"}
+filterwarnings("ignore", category=DeprecationWarning)
 
-    def __init__(self) -> None:
-        """
-
-        YouTubeMusicAPI Library
-        ~~~~~~~~~~~~~~~~~~~~~
-
-        An unofficial YouTube Music API For Python Programming Language. Written in Python, for human beings.
-        
-        >>> https://github.com/sijey-praveen/YouTube-Music-API 
-
-        """
-
-        self.user_agent = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0"}
-
-        filterwarnings("ignore", category=DeprecationWarning)
-
-    def get_track(self, query : str):
-        query = query.replace(" ", "+")
-        re = get(f"https://music.youtube.com/search?q={query}", headers=self.user_agent).content.decode("unicode_escape").split('"')
+@app.route("/", methods = ["GET"])
+@cross_origin()
+def main():
+    if request.args.get("q") == None : return "Query is None!"
+    else : query = request.args.get("q")
+    def suggestion():
+        re = get("https://music.youtube.com/search?q={}".format(query.replace(" ", "+")), headers=user_agent).content.decode("unicode_escape").split('"')
         count = 0
-
         for i in re:
             count += 1
-            if "contentPosition" == i : break
+            if "videoId" == i : id = re[count + 1]
+        return f"https://music.youtube.com/watch?v={id}"
 
-        try: 
-            count = count + 29
-            id = re[count]
-        except IndexError : print(f"No result found."), exit()
+    def url():
+        count, results = 0, get(f"https://www.youtube.com/results?q={query}", headers=user_agent).text.split('"')
+        for i in results:
+            count += 1
+            if i == "WEB_PAGE_TYPE_WATCH" : break
+        if "watch" in results[count - 5] : id = results[count - 5]
+        return f"https://music.youtube.com{id}"
 
-        data = BeautifulSoup(get(f"https://www.youtube.com/watch?v={id}").text, 'html.parser')
+    return dict(url = url(), suggested_music_video = suggestion())
 
-        return dict(
-            track_name = data.find("meta", attrs={"name" : "title"}).get("content"), 
-            datePublished = data.find("meta", attrs={"itemprop" : "datePublished"}).get("content"),
-            track_Id = id,
-            track_url = f"https://music.youtube.com/watch?v={id}",
-            track_image = f"https://i.ytimg.com/vi/{id}/hqdefault.jpg", 
-        )
-
-# t = YouTubeMusicAPI().get_track("")
-# print(t)
+app.run(host="0.0.0.0", debug=True)
