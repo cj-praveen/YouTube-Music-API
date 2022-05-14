ce560124f584850953a16bac07890c38f4e8bd1e
Sijey <cjpraveen@hotmail.com>
2022-05-14 15:08:29 +0530

    Add files via upload

--- YouTubeMusicAPI/__init__.py
+++ YouTubeMusicAPI/__init__.py
@@ -5,7 +5,6 @@ __copyright__ = "Copyright (c) 2021 Sijey"
 __license__ = "MIT" # >>> https://mit-license.org/
 
 from requests import get
-from bs4 import BeautifulSoup
 from re import search, findall
 from warnings import filterwarnings
 
@@ -22,30 +21,35 @@ class YouTubeMusicAPI():
         
     def track(self, query : str):
         id = search('"videoId":".{11}"', get(f"https://music.youtube.com/search?q={query}", headers=self.user_agent).content.decode("unicode_escape"))
-        if id == None : return "No Result Found!"
+        if id == None : return dict(message = "No Track Found!")
         else : id = id.group().split(":")[1].replace('"', "")
-        __src__ = BeautifulSoup(get(f"https://www.youtube.com/watch?v={id}").text, 'html.parser')
+        meta = get(f"https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v={id}", headers=self.user_agent).json()
         return dict(
-            name = __src__.find("meta", attrs={"name" : "title"}).get("content"), 
-            datePublished = __src__.find("meta", attrs={"itemprop" : "datePublished"}).get("content"),
+            name = meta["title"],
             Id = id,
             url = f"https://music.youtube.com/watch?v={id}",
-            image = f"https://i.ytimg.com/vi/{id}/hqdefault.jpg", 
+            image = f"https://i.ytimg.com/vi/{id}/hqdefault.jpg",
+            author_name = meta["author_name"],
+            author_url = meta["author_url"]
         )
 
     def playlist(self, query : str):
         id = search('"playlistId":".{34}"', get(f"https://www.youtube.com/results?search_query={query}&sp=EgIQAw%253D%253D", headers=self.user_agent).content.decode("utf-8"))
-        if id == None : return "No Result Found!"
+        if id == None : return dict(message = "No Playlist Found!")
         else : id = id.group().split(":")[1].replace('"', "")
+        meta = get(f"https://www.youtube.com/oembed?url=https://www.youtube.com/playlist?list={id}", headers=self.user_agent).json()
         return dict(
-            name = BeautifulSoup(get(f"https://music.youtube.com/playlist?list={id}").text, 'html.parser').find("meta", attrs={"property" : "og:title"}).get("content"),
+            name = meta["title"],
             url = f"https://music.youtube.com/playlist?list={id}",
-            Id = id
+            Id = id,
+            author_name = meta["author_name"],
+            author_url = f"https://www.youtube.com{meta['author_url']}"
         )
 
     def searchUrls(self, query : str):
         ids, result = [], findall('"videoId":".{11}"', get(f"https://music.youtube.com/search?q={query}", headers=self.user_agent).content.decode("unicode_escape"))
-        if result == None : return "No Result Found!"
+        if result == None : return dict(message = "No URLs Found!")
         for i in result : ids.append("https://music.youtube.com/watch?v={}".format(i.split(":")[1].replace('"', "")))
         return list(set(ids))
 
+print(YouTubeMusicAPI().playlist("Alan Walker"))
