6e99b7ca9c55c153156a05c8446be54d6c17c282
Sijey <cjpraveen@hotmail.com>
2022-06-12 13:00:28 +0530

    Update __init__.py

--- YouTubeMusicAPI/__init__.py
+++ YouTubeMusicAPI/__init__.py
@@ -20,7 +20,7 @@ class YouTubeMusicAPI():
         self.user_agent = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.69"}
         
     def track(self, query : str):
-        id = search('"videoId":".{11}"', get(f"https://music.youtube.com/search?q={query}", headers=self.user_agent).content.decode("unicode_escape"))
+        id = search('"videoId":"(.*?)"', get(f"https://music.youtube.com/search?q={query}", headers=self.user_agent).content.decode("unicode_escape"))
         if id == None : return dict(message = "No Track Found!")
         else : id = id.group().split(":")[1].replace('"', "")
         meta = get(f"https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v={id}", headers=self.user_agent).json()
@@ -47,9 +47,7 @@ class YouTubeMusicAPI():
         )
 
     def searchUrls(self, query : str):
-        ids, result = [], findall('"videoId":".{11}"', get(f"https://music.youtube.com/search?q={query}", headers=self.user_agent).content.decode("unicode_escape"))
+        ids, result = [], findall('"videoId":"(.*?)"', get(f"https://music.youtube.com/search?q={query}", headers=self.user_agent).content.decode("unicode_escape"))
         if result == None : return dict(message = "No URLs Found!")
         for i in result : ids.append("https://music.youtube.com/watch?v={}".format(i.split(":")[1].replace('"', "")))
         return list(set(ids))
-
-print(YouTubeMusicAPI().playlist("Alan Walker"))
