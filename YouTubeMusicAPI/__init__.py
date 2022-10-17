ebce4da3ee224cb43a9706cce139687df3d61ba6
Praveen <cjpraveen@hotmail.com>
2022-10-17 21:46:57 +0530

    Update __init__.py

--- YouTubeMusicAPI/__init__.py
+++ YouTubeMusicAPI/__init__.py
@@ -1,6 +1,7 @@
 from requests import get
 from re import search
 from warnings import filterwarnings
+from json import loads, dumps
 
 filterwarnings("ignore", category=DeprecationWarning)
 
@@ -10,21 +11,23 @@ def Search(query : str):
         "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36"
     }
 
-    id = search('"videoId":"(.*?)"', get(f"https://music.youtube.com/search?q={query}", headers=user_agent).content.decode("unicode_escape"))
+    page_source: str = get(f"https://music.youtube.com/search?q={query}", headers=user_agent).content.decode("unicode_escape")
+
+    id = search('"videoId":"(.*?)"', page_source)
 
     if id:
-        id = id.group().split(":")[1].replace('"', "")
+        id = loads(f"{{{id.group()}}}")["videoId"]
         
         meta = get(f"https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v={id}", headers=user_agent).json()
 
-        return dict(
+        return dumps(dict(
             name = meta["title"],
             Id = id,
             url = f"https://music.youtube.com/watch?v={id}",
             image = f"https://i.ytimg.com/vi/{id}/hqdefault.jpg",
             author_name = meta["author_name"],
             author_url = meta["author_url"]
-        )
+        ))
 
     else:
         return dict(message = "No Track Found!")
