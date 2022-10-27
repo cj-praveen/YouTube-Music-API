2e3cb6551d4c3d250b5b9127289f5f0431da0b24
Praveen <cjpraveen@hotmail.com>
2022-10-27 21:28:20 +0530

    Update main.py

--- web/main.py
+++ web/main.py
@@ -3,7 +3,7 @@ from uvicorn import run
 from requests import get
 from re import search
 from warnings import filterwarnings
-from json import loads, dumps
+from json import loads
 
 filterwarnings("ignore", category=DeprecationWarning)
 
@@ -12,23 +12,25 @@ app = FastAPI()
 @app.get("/")
 def main_(query: str):
     if query:
-        page_source: str = get(f"https://music.youtube.com/search?q={query}", headers={"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36"}).content.decode("unicode_escape")
+        headers: dict = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36"}
+
+        page_source: str = get(f"https://music.youtube.com/search?q={query}", headers=headers).content.decode("unicode_escape")
 
         id = search('"videoId":"(.*?)"', page_source)
 
         if id:
             id = loads(f"{{{id.group()}}}")["videoId"]
             
-            meta = get(f"https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v={id}", headers=user_agent).json()
+            meta = get(f"https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v={id}", headers=headers).json()
 
-            return dumps(dict(
+            return dict(
                 name = meta["title"],
                 Id = id,
                 url = f"https://music.youtube.com/watch?v={id}",
                 image = f"https://i.ytimg.com/vi/{id}/hqdefault.jpg",
                 author_name = meta["author_name"],
                 author_url = meta["author_url"]
-            ))
+            )
         else:
             return dict(message = "no track found!")
 
