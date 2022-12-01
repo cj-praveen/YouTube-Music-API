764424b782db5889b537a1fbe172285d55c94e00
Praveen <cjpraveen@hotmail.com>
2022-12-01 18:24:21 +0530

    Update __init__.py

--- src/YouTubeMusicAPI/__init__.py
+++ src/YouTubeMusicAPI/__init__.py
@@ -1,10 +1,9 @@
-from urllib.request import Request, urlopen
-from urllib.parse import quote
+import urllib
 import re
-from warnings import filterwarnings
-from json import loads
+import warnings
+import json
 
-filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 def Search(query: str):
 
@@ -12,12 +11,12 @@ def Search(query: str):
         "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36"
     }
 
-    page_source: str = urlopen(Request(f"https://music.youtube.com/search?q={quote(query)}", headers=headers)).read().decode("unicode_escape")
+    page_source: str = urllib.request.urlopen(urllib.request.Request(f"https://music.youtube.com/search?q={urllib.parse.quote(query)}", headers=headers)).read().decode("unicode_escape")
 
     if trackId := re.search('"videoId":"(.*?)"', page_source):
         trackId = loads(f"{{{trackId.group()}}}")["videoId"]
 
-        meta = loads(urlopen(Request(f"https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v={trackId}", headers=headers)).read())
+        meta = json.loads(urllib.request.urlopen(urllib.request.Request(f"https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v={trackId}", headers=headers)).read())
 
         return dict(
             trackName = meta["title"],
