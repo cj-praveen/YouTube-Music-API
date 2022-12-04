2bf88a17672c75cfe59d5946f331931d567ba948
Praveen <cjpraveen@hotmail.com>
2022-12-04 16:11:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+setuptools.setup(
     name="YouTubeMusicAPI",
-    version="2.7.1",
+    version="2.7.2",
     description="The search API for YouTube Music.",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/cj-praveen/YouTube-Music-API",
+    url="https://github.com/cj-praveen/YouTube-Music-API/",
     keywords = "youtube music api, YouTubeMusicAPI, python youtube music api, youtube music api python, youtube api pypi, youtube api",
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    python_requires=">=3.6",
-    license="MIT"
 )
