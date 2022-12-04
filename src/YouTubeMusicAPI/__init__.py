3f0d0416ee42d021cc630a0dc6a3e315035270e0
Praveen <cjpraveen@hotmail.com>
2022-12-04 16:16:40 +0530

    Update __init__.py

--- src/YouTubeMusicAPI/__init__.py
+++ src/YouTubeMusicAPI/__init__.py
@@ -11,9 +11,7 @@ def Search(query: str):
     The search API for YouTube Music.
     """
     
-    headers: dict = {
-        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36"
-    }
+    headers: dict = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36"}
 
     page_source: str = urllib.request.urlopen(urllib.request.Request(f"https://music.youtube.com/search?q={urllib.parse.quote(query)}", headers=headers)).read().decode("unicode_escape")
 
